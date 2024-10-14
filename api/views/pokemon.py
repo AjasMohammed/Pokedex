20ee2e6e55d8b913b3bafb83c6e919beaada3623
AjasMohammed <ajasmohammed33@gmail.com>
2024-10-14 18:59:39 +0530

    feat: Added Pagination in the 'PokemonList' view.

--- api/views/pokemon.py
+++ api/views/pokemon.py
@@ -5,8 +5,9 @@ from django.shortcuts import get_object_or_404
 
 from home.models import Pokemon
 from api.serializers.pokemon import PokemonDetailSerializer
-from django.core.paginator import Paginator
 from api.pagination import CustomPagination, PaginationHandleMixin
+
+import random
 
 
 class PokemonList(APIView, PaginationHandleMixin):
@@ -14,23 +15,32 @@ class PokemonList(APIView, PaginationHandleMixin):
     serializer_class = PokemonDetailSerializer
 
     def get(self, request):
-        pokemons = Pokemon.objects.all().prefetch_related(
-            'types', 'abilities', 'evolution_chain')
-        serializer = self.serializer_class(pokemons, many=True)
-        page = self.paginate_queryset(pokemons)
-        if page is not None:
-            serializer = self.get_paginated_response(
-                self.serializer_class(page, many=True))
-        else:
+        limit = request.GET.get('limit')
+        if limit:
+            pokemon_count = Pokemon.objects.count()
+            random_ids = random.sample(range(1, pokemon_count), int(limit))
+            pokemons = Pokemon.objects.filter(id__in=random_ids).prefetch_related(
+                'types', 'abilities', 'evolution_chain')[:int(limit)]
             serializer = self.serializer_class(pokemons, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            pokemons = Pokemon.objects.all().prefetch_related(
+                'types', 'abilities', 'evolution_chain')
+            serializer = self.serializer_class(pokemons, many=True)
+            page = self.paginate_queryset(pokemons)
+            if page is not None:
+                serializer = self.get_paginated_response(
+                    self.serializer_class(page, many=True))
+            else:
+                serializer = self.serializer_class(pokemons, many=True)
 
-        context = {
-            "next_page": serializer.data['next'],
-            "previous_page": serializer.data['previous'],
-            "count": serializer.data['count'],
-            "pokemons": serializer.data['results'].data,
-        }
-        return Response(context, status=status.HTTP_200_OK)
+            context = {
+                "next_page": serializer.data.get('next', None),
+                "previous_page": serializer.data.get('previous', None),
+                "count": serializer.data.get('count', None),
+                "pokemons": serializer.data.get('results', None).data,
+            }
+            return Response(context, status=status.HTTP_200_OK)
 
 
 class PokemonDetail(APIView):
